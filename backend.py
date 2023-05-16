5ed22fa0124f10aa8a99542d486829fb090a66ab
bro256 <bronius256@gmail.com>
2023-05-16 10:17:08 +0300

    Requirements

---
+++ backend.py
@@ -0,0 +1,6 @@
+# Reikalavimai:
+# - SQLAlchemy
+# - Duomenų modelio sąsajos: išnaudotas many-to-one ryšys, many-to-many būtų pliusas
+# - PySimpleGUI pagrindu įgyvendinta vartotojo sąsaja
+# - Išskirtas backend (modelis ir bazės sukūrimas) ir frontend (vartotojo sąsaja)
+# Objektiškai realizuota vartotojo sąsaja yra didelis pliusas 
