ddd45c977813db62476ed769bd224a8fa78c08da
bro256 <bronius256@gmail.com>
2023-05-18 22:56:43 +0300

    makaronai

--- frontend.py
+++ frontend.py
@@ -10,39 +10,35 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Retreive task objects from DB
-tasks = session.query(Task).all()
-print(tasks)
-# Create task list
-
-def get_all_tasks():
+def task_list():
     tasks = session.query(Task).all()
-    return tasks
-def prepare_task_data(tasks):
-    task_data = []
+    task_list = []
     for task in tasks:
-        task_data.append([task.id, task.task_description, task.start_date, task.finish_date,
-                          task.priority.priority_description, task.user.name, task.status.status_description])
-    return task_data
-def update_task_list():
-    tasks = get_all_tasks()
-    task_data = prepare_task_data(tasks)
-    window['-TABLE-'].update(values=task_data)
+        task_list.append([task.id, task.task_description, task.start_date, task.finish_date, 
+                        task.priority.priority_description, task.user.name, task.status.status_description])
+    return task_list
 
-task_list = []
-for task in tasks:
-    task_list.append([task.id, task.task_description, task.start_date, task.finish_date, 
-                      task.priority.priority_description, task.user.name, task.status.status_description])
+def update_task_list():
+    tasks = session.query(Task).all()
+    task_list = []
+    for task in tasks:
+        task_list.append([task.id, task.task_description, task.start_date, task.finish_date,
+                          task.priority.priority_description, task.user.name, task.status.status_description])
+    print(f'TASK DATA: {task_list}')
+    window['-TABLE-'].update(values=task_list)
+
+task_list = task_list()
 
 # Dropdown menu
 priority_list = [priority.priority_description for priority in session.query(Priority).all()]
 status_list = [status.status_description for status in session.query(Status).all()]
 user_list = [user.name for user in session.query(User).all()]
 
-# GUI
-##################################### Main Window #################################
-# Choose a Theme for the Layout
+# GUI theme
 sg.theme('LightGrey1')
+
+##################################### Main Window #################################
+# Main window layout
 layout = [
     [sg.Table(values=task_list, key='-TABLE-',
               headings=['ID', 'Description', 'Start Date', 'Finish Date', 'Priority', 'Assignee', 'Status'],
@@ -53,20 +49,18 @@ layout = [
     [sg.Button('Add Task'), sg.Button('Edit Task'), sg.Button('Delete Task'), sg.Button('Exit')],
 ]
 
-# Main window
 window = sg.Window('Task Management', layout)
 
-# Events
+# Main window events
 while True:
     event, values = window.read()
     print(f'VALUES: {values}')
-
     if event == sg.WINDOW_CLOSED or event == 'Exit':
         break
 
 ##################################### Add Task #################################
     if event == 'Add Task':
-        # Create an add task layout
+        # Add task layout
         add_layout = [
             [sg.Text('Task Description:'), sg.InputText(key='-DESCRIPTION-')],
             [sg.Text('Start Date:'), sg.InputText(key='-STARTDATE-')],
@@ -80,15 +74,12 @@ while True:
             [sg.Button('Add'), sg.Button('Cancel')],
         ]
 
-        # Create the add task window
+        # Add task window
         add_window = sg.Window('Add Task', add_layout)
-
         while True:
             add_event, add_values = add_window.read()
-
             if add_event == sg.WINDOW_CLOSED or add_event == 'Cancel':
                 break
-
             if add_event == 'Add':
                 task = Task(
                     task_description=add_values['-DESCRIPTION-'],
@@ -96,21 +87,21 @@ while True:
                     finish_date=add_values['-FINISHDATE-'],
                 )
 
-                # Check if the priority already exists
+                # Select/add PRIORITY
                 priority_description = add_values['-PRIORITY-']
                 priority = session.query(Priority).filter_by(priority_description=priority_description).first()
                 if not priority:
                     priority = Priority(priority_description=priority_description)
                 task.priority = priority
 
-                # Check if the user already exists
+                # Select/add USER
                 name = add_values['-ASSIGNEE-']
                 user = session.query(User).filter_by(name=name).first()
                 if not user:
                     user = User(name=name)
                 task.user = user
 
-                # Check if the status already exists
+                # Select/add STATUS
                 status_description = add_values['-STATUS-']
                 status = session.query(Status).filter_by(status_description=status_description).first()
                 if not status:
@@ -123,8 +114,6 @@ while True:
                 sg.popup('Task added successfully!')
                 add_window.close()
                 update_task_list()
-
-
         add_window.close()
 
 ##################################### Edit Task #################################
@@ -132,7 +121,6 @@ while True:
     if event == ('Edit Task'):
         selected_rows = values['-TABLE-']
         print(f'SELECTED ROWS: {selected_rows}')
-
         if len(selected_rows) != 1:
             sg.popup("Select one task")
         else:
@@ -141,7 +129,7 @@ while True:
             selected_task = session.query(Task).get(selected_task_id)
             print(f'SELECTED_TASK: {selected_task}')
 
-            # Create an edit task layout
+            # Edit task layout
             priority_list = [priority.priority_description for priority in session.query(Priority).all()]
             edit_layout = [
                 [sg.Text('Task Description:'), sg.InputText(selected_task.task_description, key='-DESCRIPTION-')],
@@ -156,44 +144,30 @@ while True:
                 [sg.Button('Update Task'), sg.Button('Cancel')],
             ]
 
-            # Create the edit task window
+            # Edit task window
             edit_window = sg.Window('Edit Task', edit_layout)
-
             while True:
                 edit_event, edit_values = edit_window.read()
-
                 if edit_event == sg.WINDOW_CLOSED or edit_event == 'Cancel':
                     break
-
-                # if edit_event == 'Update Task':
-                #     selected_task.task_description = edit_values['-DESCRIPTION-']
-                #     selected_task.start_date = edit_values['-STARTDATE-']
-                #     selected_task.finish_date = edit_values['-FINISHDATE-']
-                #     selected_task.priority.priority_description = edit_values['-PRIORITY-']
-                #     selected_task.user.name = edit_values['-ASSIGNEE-']
-                #     selected_task.status.status_description = edit_values['-STATUS-']
-
-                #     session.commit()
-                #     sg.popup('Task updated successfully!')
-
                 if edit_event == 'Update Task':
                     selected_task.task_description = edit_values['-DESCRIPTION-']
                     selected_task.start_date = edit_values['-STARTDATE-']
                     selected_task.finish_date = edit_values['-FINISHDATE-']
 
-                    # Update priority
+                    # Update PRIORITY
                     priority_description = edit_values['-PRIORITY-']
                     priority = session.query(Priority).filter_by(priority_description=priority_description).first()
                     if priority:
                         selected_task.priority = priority
 
-                    # Update assignee
+                    # Update ASSIGNEE
                     assignee_name = edit_values['-ASSIGNEE-']
                     assignee = session.query(User).filter_by(name=assignee_name).first()
                     if assignee:
                         selected_task.user = assignee
 
-                    # Update status
+                    # Update STATUS
                     status_description = edit_values['-STATUS-']
                     status = session.query(Status).filter_by(status_description=status_description).first()
                     if status:
@@ -202,10 +176,9 @@ while True:
                     session.commit()
                     sg.popup('Task updated successfully!')
                     edit_window.close()
-                    update_task_list()
 
-                update_task_list()
-                edit_window.close()
+            update_task_list()
+            edit_window.close()
 
 
 ##################################### Delete Task #################################
@@ -213,15 +186,15 @@ while True:
     if event == 'Delete Task':
         selected_rows = values['-TABLE-']
         print(f'SELECTED ROWS: {selected_rows}')
-        if len(selected_rows) == 0:
-            sg.popup('Please select a task to delete.')
+        if len(selected_rows) != 1:
+            sg.popup('Select one task to delete.')
         else:
             selected_task_id = int(task_list[selected_rows[0]][0])
             print(f'SELECTED_TASK_ID: {selected_task_id}')
             selected_task = session.query(Task).get(selected_task_id)
             print(f'SELECTED_TASK: {selected_task}')
 
-            confirm_msg = 'Are you sure you want to delete the selected task(s)?'
+            confirm_msg = 'Are you sure you want to delete the selected task?'
             confirm_dialog = sg.popup_yes_no(confirm_msg, title='Confirm Deletion')
             if confirm_dialog == 'Yes':
                 session.delete(selected_task)
